b0e26c9d77fc5412ace56eb1aedfc2b823f33d69
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-27 20:17:12 +0000

    Remove old-style constructor. Reformat
    git-svn-id: https://plugins.svn.wordpress.org/always-show-admin-bar/trunk@903640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- always-show-admin-bar.php
+++ always-show-admin-bar.php
@@ -10,52 +10,26 @@ Author URI: http://www.leewillis.co.uk/
 
 class asabfa {
 
-
-
-	function asabfa() {
-
-		$this->__construct();
-
-	}
-
-
-
 	function __construct() {
-
-		add_action ( 'plugins_loaded', array ( &$this, 'show_admin_bar' ) );
-		add_action ( 'init', array ( &$this, 'add_login_links' ) );
-
+		add_action( 'plugins_loaded', array( $this, 'show_admin_bar' ) );
+		add_action( 'init', array( $this, 'add_login_links' ) );
 	}
-
-
 
 	function add_login_links() {
-
 		global $wp_admin_bar;
-
 		if ($this->is_logged_in())
 			return;
-
 		if (isset($wp_admin_bar))
-			$wp_admin_bar->add_menu( array( 'id' => 'login-link', 'title' => __('Not Logged in, login here','asabfa'),  'href' => admin_url() ) );
-
+			$wp_admin_bar->add_menu( array( 'id' => 'login-link', 'title' => __( 'Not Logged in, login here', 'asabfa' ), 'href' => admin_url() ) );
 	}
-
-
 
 	function has_logged_in() {
-
 		// Have they ever logged in?
 		return isset ( $_COOKIE['asabfa_logged_in'] ) && $_COOKIE['asabfa_logged_in'] ;
-
 	}
 
-
-
 	function is_logged_in() {
-
 		$user_id = get_current_user_id();
-
 		if ( 0 != $user_id ) {
 			return TRUE;
 		} else {
@@ -63,39 +37,23 @@ class asabfa {
 		}
 	}
 
-
-
-	function show_admin_bar($show) {
-
+	function show_admin_bar( $show ) {
 		// If the user has logged in previously - show the bar
-		if ($this->has_logged_in()) {
-
+		if ( $this->has_logged_in() ) {
 			$show = TRUE;
-
 		}
-
 		// If user is currently logged in. Show the bar, and set a cookie
-		if ($this->is_logged_in()) {
-
-			setcookie ( 'asabfa_logged_in', '1', time()+60*60*24*1000 );
+		if ( $this->is_logged_in() ) {
+			setcookie( 'asabfa_logged_in', '1', time() + 60 * 60 * 24 * 1000 );
 			$show = TRUE;
-
 		}
-
-		if ($show && !is_admin()) {
-
-			add_filter ( 'show_admin_bar', '__return_true' );
-
-		} else { 
-
-			add_filter ( 'show_admin_bar', '__return_false' );
-
+		if ( $show && !is_admin() ) {
+			add_filter( 'show_admin_bar', '__return_true' );
+		} else {
+			add_filter( 'show_admin_bar', '__return_false' );
 		}
-
 	}
 
 }
 
 $asabfa = new asabfa();
-
-?>
